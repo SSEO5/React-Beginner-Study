4da80ca2e3cd355fe9c20a3b7d8ae496d16cce34
SSEO5 <wjdtjdud530@naver.com>
2023-05-09 12:16:32 +0900

    4주차

---
+++ TodoList/App.js
@@ -0,0 +1,37 @@
+import { useEffect, useState } from "react";
+
+function App() {
+	const [Todo, setTodo] = useState("");
+	const [Todos, setTodos] = useState([]);
+	const onChange = (event) => setTodo(event.target.value);
+	const onSubmit = (event) => {
+		event.preventDefault();
+		if (Todo === "") {
+			return;
+		}
+		setTodos((currentArray) => [Todo, ...currentArray]);
+		setTodo("");
+	};
+	return (
+		<div>
+			<h1>My To-do List ({Todos.length})</h1>
+			<form onSubmit={onSubmit}>
+				<input
+					value={Todo}
+					onChange={onChange}
+					type="text"
+					placeholder="Write your to do"
+				/>
+				<button>Add to do</button>
+			</form>
+			<hr />
+			<ul>
+				{Todos.map((item, index) => (
+					<li key={index}>{item}</li>
+				))}
+			</ul>
+		</div>
+	);
+}
+
+export default App;
